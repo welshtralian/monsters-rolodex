ced57f497c436b958583d78eddf5d47fd8e4d026
Welshtralian <garethhattersley+github@gmail.com>
2019-08-23 01:39:36 +0200

    Updates

---
+++ precache-manifest.1d0e3e9d53938ca23aae31bc76188b9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d47ae2d223e2fe5c19135f33382d6b9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "402982efaf4cb79006d4",
+    "url": "/monsters-rolodex/static/css/main.34d6ccf3.chunk.css"
+  },
+  {
+    "revision": "01d8317fe3a3835ac7c6",
+    "url": "/monsters-rolodex/static/js/2.f4bca13b.chunk.js"
+  },
+  {
+    "revision": "402982efaf4cb79006d4",
+    "url": "/monsters-rolodex/static/js/main.287576a9.chunk.js"
+  },
+  {
+    "revision": "9ea9c29140c1f80c8044",
+    "url": "/monsters-rolodex/static/js/runtime~main.3810dc4c.js"
+  }
+]);
